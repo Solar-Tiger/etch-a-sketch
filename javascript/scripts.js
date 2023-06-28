40ae77285be439343b3a11cd758b0e26699ec7c2
David Singleton <davidjsingleton18@gmail.com>
2023-06-28 10:20:10 -0700

    Add name to inner squares to separate from generic div element

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,6 +2,7 @@ const gridContainer = document.querySelector('.grid-container');
 
 for (let i = 0; i < 16; i++) {
   const gridDiv = document.createElement('div');
+  gridDiv.classList.add('etch-a-sketch-square');
 
   gridContainer.appendChild(gridDiv);
 }
