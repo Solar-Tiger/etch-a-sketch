fc7ab615278ebbc1c36e630029056011a73cc98a
David Singleton <davidjsingleton18@gmail.com>
2023-06-30 18:30:22 -0700

    Add individual functions for creating a default grid, a user defined size grid and filling in each suqare

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,51 +1,60 @@
 /* eslint-disable prefer-const */
 const gridContainer = document.querySelector('.grid-container');
-const changeGridSize = document.querySelector('.change-grid-size');
+const changeGridSizeBtn = document.querySelector('.change-grid-size-btn');
+let userGridSizeChoice;
 
-let gridSizeChoice = 8;
+createNewGrid();
 
-for (let i = 0; i < gridSizeChoice * gridSizeChoice; i++) {
-  const gridDiv = document.createElement('div');
-  gridDiv.classList.add('etch-a-sketch-square');
-  gridDiv.style.width = `${960 / gridSizeChoice}px`;
-  gridDiv.style.height = `${960 / gridSizeChoice}px`;
+let etchASketchSquare = document.querySelectorAll('.etch-a-sketch-square');
 
-  gridContainer.appendChild(gridDiv);
-}
+fillEtchASketchSquare();
 
-changeGridSize.addEventListener('click', () => {
-  let userGridSizeChoice = parseInt(
-    prompt('Please input a grid size number between 2 and 100')
+changeGridSizeBtn.addEventListener('click', () => {
+  userGridSizeChoice = parseInt(
+    prompt('Please pick a number between 2 and 100')
   );
-
-  if (userGridSizeChoice < 2) {
+  if (userGridSizeChoice >= 2 && userGridSizeChoice <= 100) {
+    while (gridContainer.lastElementChild) {
+      gridContainer.removeChild(gridContainer.lastElementChild);
+    }
+    createUserNewGrid(userGridSizeChoice);
+    fillEtchASketchSquare();
+  } else {
+    alert('Incorrect, try again');
     return 0;
   }
+});
 
-  if (userGridSizeChoice > 100) {
-    return 0;
-  }
-
-  for (let i = 0; i < userGridSizeChoice * userGridSizeChoice; i++) {
+function createNewGrid() {
+  let gridSize = 4;
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const gridDiv = document.createElement('div');
     gridDiv.classList.add('etch-a-sketch-square');
-    gridDiv.style.width = `${960 / userGridSizeChoice}px`;
-    gridDiv.style.height = `${960 / userGridSizeChoice}px`;
-
+    gridDiv.style.width = `${960 / gridSize}px`;
+    gridDiv.style.height = `${960 / gridSize}px`;
     gridContainer.appendChild(gridDiv);
   }
-});
+}
 
-const etchASketchSquare = document.querySelectorAll('.etch-a-sketch-square');
+function createUserNewGrid(userGridInput) {
+  for (let i = 0; i < userGridInput * userGridInput; i++) {
+    const gridDiv = document.createElement('div');
+    gridDiv.classList.add('etch-a-sketch-square');
+    gridDiv.style.width = `${960 / userGridInput}px`;
+    gridDiv.style.height = `${960 / userGridInput}px`;
+    gridContainer.appendChild(gridDiv);
+  }
+}
 
-etchASketchSquare.forEach((square) => {
-  let opacity = 0.1;
-
-  square.addEventListener('mouseenter', (e) => {
-    e.target.style.backgroundColor = `rgba(${255}, ${99}, ${71}, ${opacity})`;
-
-    if (opacity < 1) {
-      opacity += 0.1;
-    }
+function fillEtchASketchSquare() {
+  etchASketchSquare = document.querySelectorAll('.etch-a-sketch-square');
+  etchASketchSquare.forEach((square) => {
+    let opacity = 0.1;
+    square.addEventListener('mouseenter', (e) => {
+      e.target.style.backgroundColor = `rgba(${255}, ${99}, ${71}, ${opacity})`;
+      if (opacity < 1) {
+        opacity += 0.1;
+      }
+    });
   });
-});
+}
