e7d1b68f188daa068dc05550d88b6b21d3fff70d
David Singleton <davidjsingleton18@gmail.com>
2023-07-01 20:11:50 -0700

    Add functions for all touch events

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable prefer-const */
 const gridContainer = document.querySelector('.grid-square-container');
 const changeGridSizeBtn = document.querySelector('.change-grid-size-btn');
@@ -8,10 +9,12 @@ createNewGrid();
 let etchASketchSquare = document.querySelectorAll('.etch-a-sketch-square');
 
 fillEtchASketchSquare();
+fillEtchASketchSquareTouch();
 
 changeGridSizeBtn.addEventListener('click', () => {
   userGridSizeChoice = parseInt(
-    prompt('Please pick a number between 2 and 100')
+    prompt('Please pick a number between 2 and 100'),
+    10
   );
   if (userGridSizeChoice >= 2 && userGridSizeChoice <= 100) {
     while (gridContainer.lastElementChild) {
@@ -19,44 +22,11 @@ changeGridSizeBtn.addEventListener('click', () => {
     }
     createUserNewGrid(userGridSizeChoice);
     fillEtchASketchSquare();
+    fillEtchASketchSquareTouch();
   } else {
     alert('Incorrect, try again');
     return 0;
   }
-});
-
-gridContainer.addEventListener('touchstart', (e) => {
-  e.preventDefault();
-
-  changeBackgronudColor(e.target);
-});
-
-let previousSquare = null;
-
-etchASketchSquare.forEach((square) => {
-  square.addEventListener('touchmove', (e) => {
-    e.preventDefault();
-
-    const elem = document.elementFromPoint(
-      e.touches[0].clientX,
-      e.touches[0].clientY
-    );
-
-    if (elem && e.target.classList.contains('etch-a-sketch-square')) {
-      const currentSquare = elem;
-
-      if (currentSquare !== previousSquare) {
-        changeBackgronudColor(currentSquare);
-        previousSquare = currentSquare;
-      }
-    }
-  });
-});
-
-gridContainer.addEventListener('touchend', (e) => {
-  e.preventDefault();
-
-  previousSquare = null;
 });
 
 function createNewGrid() {
@@ -84,11 +54,59 @@ function fillEtchASketchSquare() {
   etchASketchSquare.forEach((square) => {
     let opacity = 0.1;
     square.addEventListener('mouseenter', (e) => {
-      e.target.style.backgroundColor = `rgba(${255}, ${99}, ${71}, ${opacity})`;
+      changeBackgronudColor(e.target);
       if (opacity < 1) {
         opacity += 0.1;
       }
     });
+  });
+}
+
+function fillEtchASketchSquareTouch() {
+  etchASketchSquare.forEach((square) => {
+    square.addEventListener('touchstart', touchStart);
+    square.addEventListener('touchmove', touchMove);
+    square.addEventListener('touchend', touchEnd);
+  });
+}
+
+function touchStart() {
+  gridContainer.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+
+    changeBackgronudColor(e.target);
+  });
+}
+
+let previousSquare = null;
+
+function touchMove() {
+  etchASketchSquare.forEach((square) => {
+    square.addEventListener('touchmove', (e) => {
+      e.preventDefault();
+
+      const elem = document.elementFromPoint(
+        e.touches[0].clientX,
+        e.touches[0].clientY
+      );
+
+      if (elem && e.target.classList.contains('etch-a-sketch-square')) {
+        const currentSquare = elem;
+
+        if (currentSquare !== previousSquare) {
+          changeBackgronudColor(currentSquare);
+          previousSquare = currentSquare;
+        }
+      }
+    });
+  });
+}
+
+function touchEnd() {
+  gridContainer.addEventListener('touchend', (e) => {
+    e.preventDefault();
+
+    previousSquare = null;
   });
 }
 
